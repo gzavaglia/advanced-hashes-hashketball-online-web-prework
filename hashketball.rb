f1921e63eacf7350295f53048cd3cebf68284c21
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-01 17:31:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,7 +20,7 @@ def game_hash
           :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2}, #end Jeff 
         "Bismak Biyombo" => {:number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 7, :blocks =>15, :slam_dunks => 10}, #end Bismak
         "DeSagna Diop" => {:number => 2, :shoe => 14, :points => 24, :rebounds =>12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5}, #end DeSagna
-        "Ben Gordon" => {:number => 8, :shoe => 19, :points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0}, #end Ben
+        "Ben Gordon" => {:number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0}, #end Ben
         "Brendan Haywood" => {:number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 22, :blocks =>5, :slam_dunks => 12}, #end Brendan
       } #end players
     }#end away 
